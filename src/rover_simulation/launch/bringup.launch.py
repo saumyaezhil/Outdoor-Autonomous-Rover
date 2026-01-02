f01a9bfeb70ee69ec425093624add653bd349cd2
Saumya Ezhil <saumya.ezhil4@gmail.com>
2026-01-02 19:59:06 +0530

    better bringup structure and usability

---
+++ src/rover_simulation/launch/bringup.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg = get_package_share_directory('rover_simulation')
+
+    return LaunchDescription([
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg, 'launch', 'sim.launch.py')
+            )
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg, 'launch', 'relays.launch.py')
+            )
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg, 'launch', 'ekf.launch.py')
+            )
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg, 'launch', 'slam.launch.py')
+            )
+        )
+    ])
