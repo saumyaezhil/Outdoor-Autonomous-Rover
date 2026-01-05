2c3a878e3780644350b4be152a6314b9d8cffccc
Saumya Ezhil <saumya.ezhil4@gmail.com>
2026-01-05 23:03:25 +0530

    Checkpoint: Gazebo + URDF + ros2_control wiring (controllers not stabilized)

--- src/rover_simulation/launch/sim.launch.py
+++ src/rover_simulation/launch/sim.launch.py
@@ -1,63 +1,47 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory("gazebo_ros"),
-                "launch",
-                "gazebo.launch.py"
-            )
-        )
-    )
-
     urdf_path = os.path.join(
-        get_package_share_directory("rover_description"),
-        "urdf",
-        "rover.urdf"
-    )
-
-    controllers = os.path.join(
-        get_package_share_directory("rover_simulation"),
-        "config",
-        "controllers.yaml"
+        os.getenv("HOME"),
+        "outdoor_rover_ws/src/rover_description/urdf/rover.urdf"
     )
 
     return LaunchDescription([
 
-        gazebo,
+        ExecuteProcess(
+            cmd=["gazebo", "--verbose", "-s", "libgazebo_ros_factory.so"],
+            output="screen"
+        ),
 
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
-            parameters=[{"robot_description": open(urdf_path).read()}]
+            parameters=[{"robot_description": open(urdf_path).read()}],
+            output="screen"
         ),
 
-        # IMPORTANT: controllers are loaded INTO Gazebo's controller_manager
+        Node(
+            package="gazebo_ros",
+            executable="spawn_entity.py",
+            arguments=["-entity", "rover", "-topic", "robot_description"],
+            output="screen"
+        ),
+
         Node(
             package="controller_manager",
             executable="spawner",
             arguments=["joint_state_broadcaster"],
-            parameters=[controllers]
+            output="screen"
         ),
 
         Node(
             package="controller_manager",
             executable="spawner",
             arguments=["diff_drive_controller"],
-            parameters=[controllers]
-        ),
-
-        Node(
-            package="gazebo_ros",
-            executable="spawn_entity.py",
-            arguments=["-entity", "rover", "-file", urdf_path],
             output="screen"
-        )
+        ),
     ])
